8ae7fce6748c0b24c81f8170d1f2cf7d0ed55a76
max <maximilian.rallis@gmail.com>
2023-02-24 19:30:54 +0100

    Hier ist ein Aktualisierungsversuch

---
+++ Test_API.py
@@ -0,0 +1,16 @@
+import requests as requests
+
+endpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos"
+# Replace DEMO_KEY below with your own key if you generated one.
+api_key = "DEMO_KEY"
+query_params = {"api_key": api_key, "earth_date": "2020-07-01"}
+response = requests.get(endpoint, params=query_params)
+response
+
+response.json()
+
+
+photos = response.json()["photos"]
+print(f"Found {len(photos)} photos")
+
+print(photos[4]["img_src"])

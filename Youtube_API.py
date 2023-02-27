86846b6aede1efbe04605820afd7f90bdfe8ad5a
maximilianrallis <118753207+maximilianrallis@users.noreply.github.com>
2023-02-27 17:00:59 +0100

    Youtube API Test

---
+++ Youtube_API.py
@@ -0,0 +1,11 @@
+from googleapiclient.discovery import build
+
+api_key = 'AIzaSyD89wd3RXw0QRrUmyvRR_XeGcCZcD0B74g'
+
+youtube = build("youtube", "v3", developerKey= api_key)
+
+request = youtube.channels().list(part= "statistics",forUsername= "schafer5")
+
+response = request.execute()
+
+print(response)

dcb06521d47ad9d80fe74fa652f382270cb7698b
max <maximilian.rallis@gmail.com>
2023-02-27 20:27:39 +0100

    Hier ist ein Aktualisierungsversuch

--- Youtube_API.py
+++ Youtube_API.py
@@ -2,7 +2,14 @@ from googleapiclient.discovery import build
 
 api_key = 'AIzaSyD89wd3RXw0QRrUmyvRR_XeGcCZcD0B74g'
 
-youtube = build("youtube", "v3", developerKey= api_key)
+youtube = build("youtube", "v3", api_key)
+
+search_response = youtube.search().list(
+    q=search_query,
+    type='video',
+    part='statistics',
+    maxResults=max_results
+                                        ).execute()
 
 request = youtube.channels().list(part= "statistics",forUsername= "schafer5")
 

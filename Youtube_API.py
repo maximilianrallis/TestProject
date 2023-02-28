83b173fd743cb2a5f7173dde808e0b58af52840f
maximilianrallis <118753207+maximilianrallis@users.noreply.github.com>
2023-02-28 10:53:48 +0100

    Hier das sollte funktionieren, wird aber von meinem Arbeitsrechner nicht zugelssen -> Zuhause probieren

--- Youtube_API.py
+++ Youtube_API.py
@@ -1,15 +1,20 @@
+import os
+os.environ['HTTP_PROXY'] = 'http://proxy.example.com:8080'
+os.environ['HTTPS_PROXY'] = 'https://proxy.example.com:8080'
+
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+
 from googleapiclient.discovery import build
 
 api_key = 'AIzaSyD89wd3RXw0QRrUmyvRR_XeGcCZcD0B74g'
 
-youtube = build("youtube", "v3", api_key)
+youtube = build("youtube", "v3", developerKey= api_key)
 
-search_response = youtube.search().list(
+request = youtube.channels().list(
+        part="statistics",
+        forUsername="schafer5",
+    )
 
-                                        ).execute()
-
-request = youtube.channels().list(part= "statistics",forUsername= "schafer5")
 
 response = request.execute()
-
-print(response)

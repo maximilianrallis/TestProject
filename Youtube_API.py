11546bc1ff86c4b08b6a4763400e722b56d3d97a
maximilianrallis <118753207+maximilianrallis@users.noreply.github.com>
2023-02-28 10:35:10 +0100

    new round at 10:35 on 28.02

--- Youtube_API.py
+++ Youtube_API.py
@@ -5,10 +5,7 @@ api_key = 'AIzaSyD89wd3RXw0QRrUmyvRR_XeGcCZcD0B74g'
 youtube = build("youtube", "v3", api_key)
 
 search_response = youtube.search().list(
-    q=search_query,
-    type='video',
-    part='statistics',
-    maxResults=max_results
+
                                         ).execute()
 
 request = youtube.channels().list(part= "statistics",forUsername= "schafer5")
